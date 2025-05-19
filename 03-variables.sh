5195fc68642f81d9d2aefc3b85ee320b9d8e0a8e
unknown <harha@gmail.com>
2025-05-19 14:57:49 +0530

    m

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,5 +6,6 @@ person2=musk
 echo "$person1: hai ra $person2"
 echo "$person2: cheppura $person2"
 ehco "$person1: car kavali ra okati $person2"
-echo "$person2: ok"
+echo "$person2: ok ra $person1"
+
  
