95b20d7666ffefe98378555ea81933d9d158fb28
unknown <harha@gmail.com>
2025-05-19 21:38:19 +0530

    mdm

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,3 +3,11 @@
 echo "All varialbes passed to the script: $@"
 
 echo "Number of variables: $#"
+
+edhco "Current directory: $PWD"
+
+echo "User running this script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10&
+echo "PID of last command in background: $!"
