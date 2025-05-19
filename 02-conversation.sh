763ad17b809b84e1e8bb755c14ecb6e1f4783ce2
unknown <harha@gmail.com>
2025-05-19 14:22:53 +0530

    hf

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
+person1=trump
+person2=musk
+
 echo "trump: hai ra musk"
 echo "musk: cheppura tump"
 ehco "trump: car kavali ra okati"
 echo "musk: ok"
+ 
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=trump
+person2=musk
+
+echo "$person1: hai ra $person2"
+echo "$person2: cheppura $person2"
+ehco "$person1: car kavali ra okati"
+echo "$person2: ok"
+ 
