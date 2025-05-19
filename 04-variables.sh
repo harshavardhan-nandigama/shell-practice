2ab5766d73948d16ccca14205df648b137e241a3
unknown <harha@gmail.com>
2025-05-19 15:40:27 +0530

    mdm

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1: hai ra $person2"
+echo "$person2: cheppura $person2"
+ehco "$person1: car kavali ra okati $person2"
+echo "$person2: ok ra $person1"
+
+ 
