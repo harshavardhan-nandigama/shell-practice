c7d458090baf409bc8e40ec1b65ed063d116d794
unknown <harha@gmail.com>
2025-05-19 16:26:01 +0530

    mdm

---
+++ 06-data-types.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER$=100
+NUMBER$=200
+
+TIMESTAMP=$(date)
+echo "script exicuted at time stamp: $timestamp1"
+
+SUM$=((($NUMBER1+$NUMBER2)))
+
+echo "sum of $NUMBER1 + $NUMBER2 sum of one $
