59eec00f344dfa6c04153bb4c6b23a0191134a95
unknown <harha@gmail.com>
2025-05-20 06:48:52 +0530

    f

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -5,8 +5,17 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then    
     echo "ERROR:: please run this script with root access"
+    exit 1 #give other than 0 upto 127
 else
     echo "you are running with root access"
 fi
 
 dnf install msql -y
+
+if [ $? -eq 0 ]
+then
+    echo "Installing MySQL is... SUCCESS"
+else 
+    echo "Installing MySQL is... FAILURE"
+    #exit 1
+fi
