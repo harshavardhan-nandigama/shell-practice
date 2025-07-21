8ed8558b216817f79159203dd7ac7ab36d8139bb
unknown <harha@gmail.com>
2025-07-21 06:39:06 +0530

    shell

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!bin/bash
 
 NUMBER1=100
 NUMBER2=200
 
 TIMESTAMP=$(date)
-echo "script exicuted at time stamp: $TIMESTAMP"
-SUM=$((($NUMBER1+$NUMBER2)))
-
-echo "SUM of $NUMBER1 + $NUMBER2 is: $sum"
+echo "script executed at: $TIMESTAMP"
+#SUM=$NUMBER1+$NUMBER2
