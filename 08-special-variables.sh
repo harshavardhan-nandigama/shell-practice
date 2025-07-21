42d5b656499d022e572ec37c448bb61a68996634
unknown <harha@gmail.com>
2025-07-21 12:28:03 +0530

    shell

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "All variables passed to the script: S@"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Current Directory: $PWD"
+echo "Home directory of user: $HOME
+echo "PID of the script of user: $HOME"
+sleep 10 &
+echo "PID of last command in background: $!"
