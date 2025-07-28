15bf909422cbf65bfaef6aa7702b08521899058f
unknown <harha@gmail.com>
2025-07-28 06:08:59 +0530

    shell

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,5 +2,8 @@
 
 MOVIES=("Court" "Hit" "Pushpa" "Thandel")
 
-echo "First Movie: ${MOVIES[0]}
+echo "First Movie: ${MOVIES[0]}"
+echo "Third Movie: ${MOVIES[2]}"
+echo "Fourth Movie:${MOVIES[3]}"
 
+echo "All Movies: ${MOVIES[@]}"
